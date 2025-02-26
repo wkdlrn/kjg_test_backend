f02b1e15588ccd77dae9d8bd9ba6eca11d3b68ab
wkdlrn <kkjjgg97@gmail.com>
2025-02-26 09:41:36 +0900

    [Feat] Board 엔티티 생성

--- src/main/java/com/example/back/board/Board.java
+++ src/main/java/com/example/back/board/Board.java
@@ -1,4 +1,26 @@
 package com.example.back.board;
 
+import com.example.back.comment.Comment;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
 public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idx;
+    private String title;
+    private String content;
+    private String writer;
+
 }
