57c345d785727a14aebac3b0069cc1e47f072bc6
wkdlrn <kkjjgg97@gmail.com>
2025-02-26 10:17:59 +0900

    [Feat] BoardService 생성

--- src/main/java/com/example/back/board/BoardDto.java
+++ src/main/java/com/example/back/board/BoardDto.java
@@ -5,16 +5,17 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 public class BoardDto {
-    @Getter
-    @Builder
-    @AllArgsConstructor
-    @NoArgsConstructor
+    @Getter @Builder @AllArgsConstructor @NoArgsConstructor
     public static class RegisterReq {
         private String title;
         private String content;
         private String writer;
-
         public Board toEntity() {
             return Board.builder()
                     .title(title)
@@ -22,25 +23,20 @@ public class BoardDto {
                     .writer(writer)
                     .build();
         }
-
-        @Getter
-        @Builder
-        @AllArgsConstructor
-        @NoArgsConstructor
-        public static class RegisterResp {
-            private Long idx;
-            private String title;
-            private String content;
-            private String writer;
-
-            public static RegisterResp fromEntity(Board board) {
-                return RegisterResp.builder()
-                        .idx(board.getIdx())
-                        .title(board.getTitle())
-                        .content(board.getContent())
-                        .writer(board.getWriter())
-                        .build();
-            }
+    }
+    @Getter @Builder @AllArgsConstructor @NoArgsConstructor
+    public static class RegisterResp {
+        private Long idx;
+        private String title;
+        private String content;
+        private String writer;
+        public static RegisterResp fromEntity(Board board) {
+            return RegisterResp.builder()
+                    .idx(board.getIdx())
+                    .title(board.getTitle())
+                    .content(board.getContent())
+                    .writer(board.getWriter())
+                    .build();
         }
     }
 }
--- src/main/java/com/example/back/board/BoardService.java
+++ src/main/java/com/example/back/board/BoardService.java
@@ -1,4 +1,28 @@
 package com.example.back.board;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
 public class BoardService {
+    private final BoardRepository boardRepository;
+
+    public BoardDto.RegisterResp register (BoardDto.RegisterReq dto) {
+        Board board = boardRepository.save(dto.toEntity());
+
+        return BoardDto.RegisterResp.fromEntity(board);
+    }
+
+    public BoardDto.RegisterResp read (Long idx) {
+        return BoardDto.RegisterResp.fromEntity(boardRepository.findById(idx).orElseThrow());
+    }
+
+    public List<BoardDto.RegisterResp> readAll() {
+        return boardRepository.findAll().stream()
+                .map(BoardDto.RegisterResp::fromEntity).collect(Collectors.toList());
+    }
 }
